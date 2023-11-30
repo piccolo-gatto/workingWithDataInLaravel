e1d30e19c80de8731712fb3ee8b4565deeced929
Ekaterina <108530800+piccolo-gatto@users.noreply.github.com>
2023-11-30 12:44:04 +0800

    Fix migrate

--- database/migrations/2023_11_27_091358_soft_delete_records.php
+++ database/migrations/2023_11_27_091358_soft_delete_records.php
@@ -1,24 +1,21 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class SoftDeleteRecords extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-        //
+        Schema::table('records', function (Blueprint $table) {
+            $table->softDeletes();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
-        //
+        Schema::table('records', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
     }
-};
+}
